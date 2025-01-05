0c0084a6ad731cd3eaa546a37d384f6b1ec93c43
MohammedAbdulAhadSaud <149686654+MohammedAbdulAhadSaud@users.noreply.github.com>
2025-01-05 20:00:56 +0530

    106SeleS.c

--- 106SeleS.c
+++ 106SeleS.c
@@ -1,13 +1,10 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 void selectionSort(int a[], int n) {
     int i, j, min_idx, temp;
 
     for (i = 0; i < n - 1; i++) {
-       
         min_idx = i;
         for (j = i + 1; j < n; j++) {
             if (a[j] < a[min_idx]) {
@@ -15,7 +12,7 @@ void selectionSort(int a[], int n) {
             }
         }
 
-      
+       
         if (min_idx != i) {
             temp = a[i];
             a[i] = a[min_idx];
@@ -34,36 +31,26 @@ void printArr(int a[], int n) {
 
 int main() {
     int n;
-
- 
-    printf("Enter the size of the array: ");
+    int a[10];
+  
+    printf("Enter the size of the array(10 MAX): ");
     scanf("%d", &n);
-
-   
-    int *a = (int *)malloc(n * sizeof(int));
-    if (a == NULL) {
-        printf("Memory allocation failed!\n");
-        return 1;
-    }
-
- 
     printf("Enter %d elements: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-   
     printf("Before sorting, array elements are:\n");
     printArr(a, n);
 
- 
+
     selectionSort(a, n);
 
-   
+    
     printf("After sorting, array elements are:\n");
     printArr(a, n);
 
    
-    free(a);
+   
     return 0;
 }
